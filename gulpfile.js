38f90fc6a56c3750befe705da4ee031aa1a5a7ca
Aaron <ifengzp@163.com>
2019-02-23 18:29:34 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var babel = require('gulp-babel');
+
+gulp.task('minify-css', () => {
+    return gulp.src('./public/**/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('minify-html', () => {
+    return gulp.src('./public/**/*.html')
+      .pipe(htmlmin({ 
+          collapseWhitespace: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        }))
+      .pipe(gulp.dest('./public'))
+  });
+
+gulp.task('minify-js', function () {
+    return gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('default', gulp.series('minify-html', 'minify-css', 'minify-js'));
